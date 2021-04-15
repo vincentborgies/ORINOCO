4581d7b6d9e0070e4738f79e46c3676c22be4d24
vincentborgies <vincentborgies1@gmail.com>
2021-04-15 15:36:10 +0200

    last modifs

--- src/js/panier.js
+++ src/js/panier.js
@@ -13,7 +13,7 @@ const displayEmptyCart = () => {
 
   document.querySelector('#purchase-form-container').style.display = 'none';
   document.querySelector('.order-summary').style.paddingBottom = '300px';
-};
+}
 
 const displayCart = () => {
   cart.forEach(item => {
@@ -30,65 +30,66 @@ const displayCart = () => {
         <td class="detail-product">${item.price} €</td>
         <td class="detail-product">${totalPerProduct}</td>
     `;
-    document.querySelector('#table-products__body').prepend(displayCart);
+    document.querySelector('#table-products__body').prepend(displayCart)
   });
 
-  totalPrice = cart.reduce((acc, cur) => acc + cur.price * cur.quantity, 0);
+  totalPrice = cart.reduce((acc, cur) => acc + cur.price*cur.quantity, 0);
 
   document.querySelector('#totalPrice').textContent = `${totalPrice}€`;
-};
+}
 
 document.querySelector('#delete-cart__button').addEventListener('click', () => {
   localStorage.removeItem('cart');
   displayEmptyCart();
-  alert('les articles du panier ont été supprimés');
-});
+  alert('les articles du panier ont été supprimés')
+})
 
 if (!cart) {
-  displayEmptyCart();
+  displayEmptyCart()
 }
 
 displayCart();
 
-const inputs = document.querySelectorAll('form input');
-const confirmPurchase = document.getElementById('confirm-purchase__button');
+const inputs = document.querySelectorAll('form input')
+const confirmPurchase = document.getElementById('confirm-purchase__button')
 
 inputs.forEach(input => {
   input.addEventListener('change', event => {
     event.target.style.border = '1px solid green';
-    event.target.style.boxShadow = '#00800066 0px 0px 4px';
-  });
+    event.target.style.boxShadow = '#00800066 0px 0px 4px'
+  })
 
   input.addEventListener('invalid', event => {
     event.target.style.border = '1px solid red';
     event.target.style.boxShadow = 'rgba(128, 0, 0, 0.4) 0px 0px 4px';
-  });
+  })
+})
 
 //Création de message personnalisé pour une saisie invalide
 
 document.getElementById('firstName').addEventListener('invalid', event => {
-  event.target.setCustomValidity("Veuiller rensegner un format correct ex: Jean");
+  event.target.setCustomValidity("Veuiller renseigner un format correct ex: Jean");
 });
 
 document.getElementById('lastName').addEventListener('invalid', event => {
-  event.target.setCustomValidity("Veuillez entrer un format correct ex: Dupont");
+  event.target.setCustomValidity("Veuillez renseigner un format correct ex: Dupont");
 });
 
 document.getElementById('adress').addEventListener('invalid', event => {
-  event.target.setCustomValidity("Veuillez entrer une adresse postable correcte ex: 17 rue de la paix");
+  event.target.setCustomValidity("Veuillez renseigner une adresse postable correcte ex: 17 rue de la paix")
 });
 
 document.getElementById('zipcode').addEventListener('invalid', event => {
-  event.target.setCustomValidity("Veuillez entrer un code postal valide ex: 75000");
+  event.target.setCustomValidity("Veuillez renseigner un code postal valide ex: 75000");
 });
 
 document.getElementById('email').addEventListener('invalid', event => {
-  event.target.setCustomValidity("veuillez entrer une adresse e-mail valide ex: francoisdupont@hotmail.fr");
+  event.target.setCustomValidity("veuillez renseigner une adresse e-mail valide ex: francoisdupont@hotmail.fr");
 });
 
 document.getElementById('city').addEventListener('invalid', event => {
-  event.target.setCustomValidity("Veuillez entrer un nom de ville correct ex: Paris");
-});
+  event.target.setCustomValidity("Veuillez renseigner un nom de ville correct ex: Paris");
+})
 
 const sendOrder = async () => {
 
@@ -110,20 +111,21 @@ const sendOrder = async () => {
       email: email,
     },
     products: products,
-  };
+  }
 
   const requestOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json; charset=utf-8' },
     body: JSON.stringify(order)
-  };
+  }
 
-
-  let response = await fetch('http://localhost:3000/api/teddies/order', requestOptions);
+  const response = await fetch('http://localhost:3000/api/teddies/order', requestOptions);
   const data = await response.json();
-  console.log(data);
   window.location.href = `confirmation.html?orderId=${data.orderId}&totalPrice=${totalPrice}`;
   localStorage.removeItem('cart');
-};
+}
 
-confirmPurchase.addEventListener('submit', sendOrder())
+confirmPurchase.addEventListener('submit', e => {
+  e.preventDefault();
+  sendOrder();
+})
